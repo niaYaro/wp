3b94f580b8440e6fa51127ee2d4f69aab2271f10
Ivan Yarovyi <i.yarovyi.proj@gmail.com>
2023-10-17 12:49:17 +0300

    added cosino chips on header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Logo from '../Logo/Logo';
 import styles from '../Header/Header.module.css'
+import CasinoChip from '../CasinoChip/CasinoChip';
 
 const Header = () => {
   return (
     <div className={styles['container']}>
       <Logo />
+      <CasinoChip />
     </div>
   );
 }
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import logo from '../../img/Wonder-play-13-10-2023.png'
+import styles from '../Logo/Logo.module.css'
 
 const Logo = () => {
   return (
-    <div>
-      <img src={logo} alt="" />
+    <div className={styles['logoContainer']}>
+      <img className={styles['logo']} src={logo} alt="" />
     </div>
   );
 }
